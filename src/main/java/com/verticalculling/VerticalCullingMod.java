488f589e61fbd421689dd7ea373d3bd610ddb65d
pabloxkorn <mueblesgermandiaz@gmail.com>
2026-03-15 19:26:03 -0300

    Create VerticalCullingMod.java

---
+++ src/main/java/com/verticalculling/VerticalCullingMod.java
@@ -0,0 +1,12 @@
+package com.verticalculling;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VerticalCullingMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Vertical Culling mod cargado");
+    }
+
+}
